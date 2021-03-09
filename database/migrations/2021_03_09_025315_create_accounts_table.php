e6221a784c5413609906df120ffbef61c98ca4ad
gregorydwyer <41599082+gregorydwyer@users.noreply.github.com>
2021-03-09 12:10:14 -0500

    need to pull

---
+++ database/migrations/2021_03_09_025315_create_accounts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('platformID');
+            $table->string('username');
+            $table->string('platform');
+            $table->boolean('isVerified')->default(false);
+            $table->timestamps();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
