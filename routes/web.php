eb9547c6c9a643b1da3cb8676eed896f40b34241
gregorydwyer <41599082+gregorydwyer@users.noreply.github.com>
2021-03-16 09:51:30 -0400

    Account class added, username added to registration, email verification setup

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes(['verify' => true]);
+
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::view('/welcome', 'welcome');
 Route::view('/aboutus', 'aboutus');
 Route::get('/steamapitests', [TestsController::class, 'index'])->name('steamapitests');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
