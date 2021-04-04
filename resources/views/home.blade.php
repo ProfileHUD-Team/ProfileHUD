a2b5808eb6266fd9c8c80ee463e9c6070b09f36d
gregorydwyer <41599082+gregorydwyer@users.noreply.github.com>
2021-04-04 07:38:08 -0400

    linked account page and home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,9 +8,15 @@
 
 @section('content')
 
-    <div class="container">
-        <div class="row justify-content-center align-middle"">
-        <div class="col-md-8">
+    <div class="container flex">
+
+        <div class="row justify-content-center align-middle">
+            <div class="col-md-2 pt-1">
+                <form action="a/create">
+                    <button class="btn btn-primary" >Add Profile</button>
+                </form>
+            </div>
+            <div class="col-md-8">
             <div class="card">
                 <div class="card-header text-center">Gaming Profiles</div>
                 <div class="card-body">
@@ -20,15 +26,11 @@
                         </div>
                     @endif
                     <button class="accordion">Steam</button>
-                    <div class="panel">
-                        <div style="padding-top: 5px; padding-bottom: 5px">
-                            <div class="text-center">
-                                <form action='steamredirect'>
-                                    <input type="image" src="https://community.cloudflare.steamstatic.com/public/images/signinthroughsteam/sits_02.png" style ="height: 100px; width: 150px">
-                                </form>
+                        <div class="panel">
+                            <div style="padding-top: 5px; padding-bottom: 5px">
+                                In Progress...
                             </div>
                         </div>
-                    </div>
                     <button class="accordion">Xbox</button>
                     <div class="panel">
                         <div style="padding-top: 5px; padding-bottom: 5px">
