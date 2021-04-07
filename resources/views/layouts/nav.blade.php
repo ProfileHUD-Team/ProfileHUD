3fc4abe1e9a512c1a61c5ed55ad7fa7efbf6e56d
Willie Holmes <wlholmes@Uncg.edu>
2021-04-07 19:00:22 -0400

    logo

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
-            ProfileHUD
+            <img src="/svg/test2.svg" class="img-fluid" style ="height: 65px; width: 92px">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
