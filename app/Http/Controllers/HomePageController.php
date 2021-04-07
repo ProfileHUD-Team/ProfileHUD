49548e177a62275fec819bb2580a3b46a28706b6
Willie Holmes <wlholmes@Uncg.edu>
2021-04-07 02:30:55 -0400

    work in progress

---
+++ app/Http/Controllers/HomePageController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Account;
+use Illuminate\Http\Request;
+
+
+/**
+ * Class SteamPagesController: Controller class for hompage
+ * Date Created: 4/04/2021
+ * @author Willie Holmes
+ *
+ */
+
+class HomePageController extends Controller
+{
+    public function show($Account)
+    {
+        $Account = Account::findOrFail($Account);
+        $user = user::find($user);
+        $plays = plays::find($plays);
+        $achieves = achieves::find ($achieves);
+
+        return view('home', [
+            'Account'=> $Account,
+            'user'=> $user,
+            'plays'=> $plays,
+            'achieves'=> $achieves
+
+        ]);
+    }
+}
