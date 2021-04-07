ccf346e9e95fac5e2b30ad7c9f80bb064f8916a3
gregorydwyer <41599082+gregorydwyer@users.noreply.github.com>
2021-04-06 23:19:57 -0400

    implemented XboxAPIConnector

--- app/XboxApi/getuserid.php
+++ app/XboxApi/getuserid.php
@@ -19,11 +19,10 @@ class getuserid
 
         $url = Http::withHeaders([
 
-            'X-Auth' => '844c90481c37b3bbbdd6a2ebab654ac9cb14d377'
+            'X-Auth' => '9ae5db1e3b8c6b049fe393f7a89c11014a4c78d4'//'844c90481c37b3bbbdd6a2ebab654ac9cb14d377'
 
 
         ])->get('https://xapi.us/v2/xuid/' . $gamertag);
-
 
         return view('getXboxProfile', ['user' => $url]);
     }
