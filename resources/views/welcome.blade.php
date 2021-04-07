6cf9e44cc6f9e68ffd2e0e14aeda5ad1484a87b8
Willie Holmes <wlholmes@Uncg.edu>
2021-04-07 18:37:53 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
 
             <div class="relative flex justify-center min-h-screen sm:items-center">
 
-                    <img src="/svg/profilehud-logo.svg" class="img-fluid" style ="height: 306.5px; width: 700px">
+                    <img src="/svg/test.svg" class="img-fluid" style ="height: 650px; width: 925px">
 
             </div>
 
