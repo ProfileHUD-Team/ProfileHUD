da5f963abc4dd2982900ae6ec5d857023ca65d04
gregorydwyer <41599082+gregorydwyer@users.noreply.github.com>
2021-04-20 11:26:21 -0400

    updated welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,19 @@
 
     <div class="row justify-content-center">
 
-        <img src="/svg/test.svg" class="img-fluid" style ="height: 350px; width: 600px">
-
+        <img src="/svg/test.svg" class="img-fluid mw-100" style =" min-width: 200px">
     </div>
 
+    <div class="text-center">
+        <h5>
+            This is your one-stop destination for viewing and tracking your gaming achievements.
+            <br/>
+            View all of your games and see how many achivements you have completed!</h5>
+        <img src="/svg/home_example.png" class="img-fluid mw-100" style="">
+        <h5><br/>
+           Check which achievements still need to be completed, and how to earn them!
+        </h5>
+        <img src="/svg/game_example.png" class="img-fluid mw-100" style="">
+    </div>
 
 @endsection
