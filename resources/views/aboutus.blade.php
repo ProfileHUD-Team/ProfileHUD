a45c374682514aba12743c5b880d9cabba372ddb
gregorydwyer <41599082+gregorydwyer@users.noreply.github.com>
2021-04-19 20:27:53 -0400

    Added images to home page, added logo to tab

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -3,18 +3,20 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-10">
                 <div class="card">
-                    <div class="card-header">{{ __('About Us') }}</div>
+                    <div class="card-header text-center">{{ __('About Us') }}</div>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        {{ __('With so many services and platforms for playing video games, it can be difficult to get an idea of what games you have played on what systems. ProfileHUD aims to connect a player’s information from multiple sources such as Xbox Live, PlayStation Network and Steam. The information can then be presented in one easily accessible location. ProfileHUD will offer a one-stop web destination for tracking and viewing the player’s gaming history. Players will be able to use the service as a logbook for what games have been played and which achievements have been completed.') }}
+                    <div class="card-body text-center">
+                        With so many services and platforms for playing video games, it can be difficult to track
+                        what games you have played on what systems. ProfileHUD aims to connect a player’s information
+                        from multiple sources such as Xbox Live, PlayStation Network and Steam. The information can then
+                        be presented in one easily accessible location. ProfileHUD offers a one-stop web destination
+                        for tracking and viewing a player’s gaming history, providing a logbook for what games have been
+                        played, which achievements have been earned, and which ones are still waiting to be completed!
+                        <br/>
+                        <br/>
+                        Due to recent changes in PlayStation's system, integration of PlayStation Network accounts has been put on hold.
                     </div>
                 </div>
             </div>
